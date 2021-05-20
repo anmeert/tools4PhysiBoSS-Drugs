620640fbefffe851f11c99622cb86e4bc3697b6a
anmeert <annikameert@protonmail.com>
2021-05-20 11:37:46 +0200

    change to medians and calculate log2 ratio

--- validation_simulation_AUC.py
+++ validation_simulation_AUC.py
@@ -6,8 +6,12 @@ import os, re, sys
 import numpy as np
 import pandas as pd
 
-drug_dataframe = pd.read_csv("./data/validation_Selumetinib_30uM.csv")
-drug_df_filtered = drug_dataframe[['drug_1',  'conc_1', 'conc_2', 'log2_auc_live_ratio']]
-drug_df_averages = drug_df_filtered.groupby(['drug_1', 'conc_1', 'conc_2']).mean()
-drug_df_averages = drug_df_averages.reset_index()
-print(drug_df_averages)
+drug_dataframe = pd.read_csv("./data/validation_Pictilisib_10uM.csv")
+drug_df_filtered = drug_dataframe[['drug_1',  'conc_1', 'conc_2', 'auc_live']]
+drug_df_medians = drug_df_filtered.groupby(['drug_1', 'conc_1', 'conc_2']).median()
+drug_df_medians = drug_df_medians.reset_index()
+print(drug_df_medians)
+drug_auc = drug_df_medians.loc[drug_df_medians["conc_1"] != "None"].iloc[0]["auc_live"]
+no_drug_auc = drug_df_medians.loc[drug_df_medians["conc_1"] == "None"].iloc[0]["auc_live"]
+log2_auc_ratio = np.log2(drug_auc/no_drug_auc)
+print(log2_auc_ratio)
