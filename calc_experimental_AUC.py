bc84e86392e7eeee1a624be08e97c629bff4f9c9
anmeert <annikameert@protonmail.com>
2021-07-05 10:41:02 +0200

    normalize experimental data

--- calc_experimental_AUC.py
+++ calc_experimental_AUC.py
@@ -9,32 +9,32 @@ data.head()
 # %%
 pd.set_option("Display.max_rows", None)
 # print(list(data.columns))
-print(data.drug.unique())
 # retrieve the wildtype dataframe
 wildtype_df = data.loc[(data["drug"] == "Control") & (data["time2"] < 15.0) & (data["time2"] > 0)]
-print(wildtype_df)
+#print(wildtype_df)
 
 # retrieve the dataframe for Pictilisib
 picti_df = data.loc[(data["drug"] == "Pictilisib") & (data["uM"] == 10.0) & (data["time2"] < 15.0) & (data["time2"] > 0)]
-# print(picti_df.iloc[0:500])
-
+picti_norm = 1 - picti_df["mean"].iloc[0]
+picti_df["mean_norm"] = picti_df["mean"] + picti_norm
 # retrieve dataframe for HSPs target 
 HSPs_df = data.loc[(data["drug"] == "NMS-E973") & (data["uM"] == 3.3) & (data["time2"] < 15.0) & (data["time2"] > 0)]
-# print(HSPs_df)
-
+HSPs_norm = 1 - HSPs_df["mean"].iloc[0]
+HSPs_df["mean_norm"] = HSPs_df["mean"] + HSPs_norm
 # retrieve data for Selumetinib
 selum_df = data.loc[(data["drug"] == "Selumetinib") & (data["uM"] == 10.0) & (data["time2"] < 15.0) & (data["time2"] > 0)]
+selum_norm = 1 - selum_df["mean"].iloc[0]
+selum_df["mean_norm"] = selum_df["mean"] + selum_norm
 
 # %%
 # calculate the log2(auc_ratio) for each drug 
-print(picti_df["mean"])
 auc_wildtype = np.trapz(wildtype_df["mean"], wildtype_df["time2"])
 print(auc_wildtype)
-auc_picti = np.trapz(picti_df["mean"], picti_df["time2"])
+auc_picti = np.trapz(picti_df["mean_norm"], picti_df["time2"])
 print("Pictilisib AUC:" + str(auc_picti))
-auc_HSPs = np.trapz(HSPs_df["mean"], HSPs_df["time2"])
+auc_HSPs = np.trapz(HSPs_df["mean_norm"], HSPs_df["time2"])
 print("Luminespib AUC:" + str(auc_HSPs))
-auc_selum = np.trapz(selum_df["mean"], selum_df["time2"])
+auc_selum = np.trapz(selum_df["mean_norm"], selum_df["time2"])
 print("Selumetinib AUC:" + str(auc_selum))
 
 log2_auc_ratio_picti = np.log2(auc_picti/auc_wildtype)
